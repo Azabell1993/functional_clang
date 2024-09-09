a43731ad54de803dac1d2661f6c7afd1d0943db6
Azabell1993 <Azabell1993>
2024-09-09 23:28:27 +0900

    modify smartpointer

--- smartPointer/smartmain.c
+++ smartPointer/smartmain.c
@@ -10,130 +10,9 @@
 #include <arpa/inet.h>
 #include <stdarg.h>
 #include <error.h>
+#include "smartptr.h"
 
 #define MAX_STRING_SIZE 100
-
-// 스마트 포인터 관련 정의
-typedef struct SmartPtr {
-    void *ptr;
-    int *ref_count;
-    pthread_mutex_t *mutex;
-} SmartPtr;
-
-#define CREATE_SMART_PTR(type, ...) create_smart_ptr(sizeof(type), __VA_ARGS__)
-
-// 네트워크 정보 구조체
-typedef struct {
-    char ip[INET_ADDRSTRLEN];  // IPv4 주소를 저장
-    sa_family_t family;        // 주소 패밀리 (AF_INET 등)
-} NetworkInfo;
-
-// 네트워크 정보를 얻는 함수
-NetworkInfo get_local_network_info() {
-    struct addrinfo hints, *res;
-    NetworkInfo net_info;
-
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;  // IPv4
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;  // 로컬 호스트를 위한 IP 주소 찾기
-
-    // 호스트 이름을 통해 네트워크 정보 얻기
-    char hostname[256];
-    gethostname(hostname, sizeof(hostname));
-    if (getaddrinfo(hostname, NULL, &hints, &res) != 0) {
-        perror("getaddrinfo 실패");
-        exit(EXIT_FAILURE);
-    }
-
-    // 네트워크 정보 저장 (IPv4 주소)
-    struct sockaddr_in *ipv4 = (struct sockaddr_in *)res->ai_addr;
-    inet_ntop(AF_INET, &(ipv4->sin_addr), net_info.ip, INET_ADDRSTRLEN);
-    net_info.family = res->ai_family;
-
-    freeaddrinfo(res);  // 동적 할당된 메모리 해제
-    return net_info;
-}
-
-// 스마트 포인터 생성 함수 (가변 인자 사용)
-SmartPtr create_smart_ptr(size_t size, ...) {
-    SmartPtr sp;
-    sp.ptr = malloc(size);
-    sp.ref_count = (int *)malloc(sizeof(int));
-    *(sp.ref_count) = 1;
-    sp.mutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
-    pthread_mutex_init(sp.mutex, NULL);
-
-    // 가변 인자를 통해 초기화
-    va_list args;
-    va_start(args, size);
-
-    if (size == sizeof(int)) {
-        int value = va_arg(args, int);
-        *(int *)sp.ptr = value;
-    } else if (size == sizeof(char) * MAX_STRING_SIZE) {
-        const char *str = va_arg(args, const char *);
-        strncpy((char *)sp.ptr, str, MAX_STRING_SIZE);
-    }
-
-    va_end(args);
-    return sp;
-}
-
-// 참조 카운트 증가
-void retain(SmartPtr *sp) {
-    pthread_mutex_lock(sp->mutex);
-    (*(sp->ref_count))++;
-    pthread_mutex_unlock(sp->mutex);
-}
-
-// 참조 카운트 감소 및 메모리 해제
-void release(SmartPtr *sp) {
-    int should_free = 0;
-
-    pthread_mutex_lock(sp->mutex);
-    (*(sp->ref_count))--;
-    printf("Smart pointer released (ref_count: %d)\n", *(sp->ref_count));
-
-    if (*(sp->ref_count) == 0) {
-        should_free = 1;
-        printf("Reference count is 0, freeing memory...\n");
-    } else {
-        // Reference count is not 0, do not free memory
-        printf("Reference count is not 0, not freeing memory\n");
-    }
-
-    pthread_mutex_unlock(sp->mutex);
-
-    if (should_free) {
-        free(sp->ptr);
-        sp->ptr = NULL;  // 포인터를 NULL로 설정하여 중복 해제 방지
-        free(sp->ref_count);
-        sp->ref_count = NULL;  // ref_count도 NULL로 설정
-
-        pthread_mutex_destroy(sp->mutex);
-        free(sp->mutex);
-        sp->mutex = NULL;  // mutex 포인터도 NULL로 설정
-        printf("Memory has been freed\n");
-    }
-}
-
-// 스레드에서 사용할 함수 예시
-void* thread_function(void* arg) {
-    int thread_num = *((int*)arg);
-
-    // 네트워크 정보 가져오기
-    NetworkInfo net_info = get_local_network_info();
-
-    // 스레드 시작 메시지
-    printf("Thread %d: 시작 - 로컬 IP 주소: %s\n", thread_num, net_info.ip);
-
-    sleep(1); // 작업을 모방하기 위한 대기 시간
-
-    // 스레드 종료 메시지
-    printf("Thread %d: 종료 - 주소 패밀리: %d\n", thread_num, net_info.family);
-    return NULL;
-}
 
 // 메인 함수 - 스마트 포인터 사용 예시
 int main() {
@@ -142,7 +21,7 @@ int main() {
     char strsum[1024] = "";
 
     // 사용자로부터 생성할 스레드 개수를 입력받습니다.
-    printf("생성할 스레드의 수를 입력하세요: ");
+    safe_kernel_printf("생성할 스레드의 수를 입력하세요: ");
     scanf("%d", &num_threads);
 
     // 스레드 포인터와 스레드 번호 동적 할당
@@ -163,7 +42,7 @@ int main() {
         thread_ids[i] = i + 1;
 
         // 사용자로부터 int_num 입력받기
-        printf("스레드 %d에서 사용할 int_num 값을 입력하세요: ", thread_ids[i]);
+        safe_kernel_printf("스레드 %d에서 사용할 int_num 값을 입력하세요: ", thread_ids[i]);
         scanf("%d", &int_num_values[i]);
     }
 
@@ -172,7 +51,7 @@ int main() {
         thread_ids[i] = i + 1;
 
         // 사용자로부터 char_ptr1 입력받기
-        printf("스레드 %d에서 사용할 문자열을 입력하세요: ", thread_ids[i]);
+        safe_kernel_printf("스레드 %d에서 사용할 문자열을 입력하세요: ", thread_ids[i]);
         scanf(" %[^\n]%*c", char_ptr1_values[i]);
     }
 
@@ -194,7 +73,7 @@ int main() {
         // 스마트 포인터 해제 (참조 카운트 감소)
         release(&int_num);
     }
-    printf("결과 : %d\n", sum);
+    safe_kernel_printf("결과 : %d\n", sum);
 
     // 모든 스레드가 종료될 때까지 대기
     for (int i = 0; i < num_threads; i++) {
@@ -207,37 +86,37 @@ int main() {
         retain(&char_ptr1);
 
         // 스레드 종료 메시지 출력
-        printf("스레드 %d: %s\n", thread_ids[i], (char *)char_ptr1.ptr);
+        safe_kernel_printf("스레드 %d: %s\n", thread_ids[i], (char *)char_ptr1.ptr);
         strcat(strsum, (char *)char_ptr1.ptr);
 
         // 스마트 포인터 해제 (참조 카운트 감소)
         release(&char_ptr1);
     }
-    printf("결과 : %s\n", strsum);
+    safe_kernel_printf("결과 : %s\n", strsum);
 
     // 동적으로 할당된 스레드 포인터와 스레드 ID 배열 해제
     if (threads != NULL) {
         free(threads);
         threads = NULL;
-        printf("스레드 포인터 메모리 해제 완료\n");
+        safe_kernel_printf("스레드 포인터 메모리 해제 완료\n");
     }
 
     if (thread_ids != NULL) {
         free(thread_ids);
         thread_ids = NULL;
-        printf("스레드 ID 배열 메모리 해제 완료\n");
+        safe_kernel_printf("스레드 ID 배열 메모리 해제 완료\n");
     }
 
     if (int_num_values != NULL) {
         free(int_num_values);
         int_num_values = NULL;
-        printf("int_num_values 배열 메모리 해제 완료\n");
+        safe_kernel_printf("int_num_values 배열 메모리 해제 완료\n");
     }
 
     if (char_ptr1_values != NULL) {
         free(char_ptr1_values);
         char_ptr1_values = NULL;
-        printf("char_ptr1_values 배열 메모리 해제 완료\n");
+        safe_kernel_printf("char_ptr1_values 배열 메모리 해제 완료\n");
     }
 
 
--- smartPointer/smartptr.h
+++ smartPointer/smartptr.h
@@ -27,7 +27,6 @@ static void retain(SmartPtr *sp);
 static void release(SmartPtr *sp);
 static void safe_kernel_printf(const char *format, ...);
 static void kernel_errExit(const char *format, ...);
-static void outputError(bool useErr, int err, bool flushStdout, const char *format, va_list ap);
 static void kernel_socket_communication(int sock_fd, const char *message, char *response, size_t response_size);
 static void kernel_create_thread(pthread_t *thread, void *(*start_routine)(void *), void *arg);
 static void* thread_function(void* arg);
@@ -37,51 +36,67 @@ static void kernel_wait_for_process(pid_t pid);
 
 static pthread_mutex_t print_mutex = PTHREAD_MUTEX_INITIALIZER;
 
+// 스마트 포인터 관련 정의
 typedef struct SmartPtr {
     void *ptr;
     int *ref_count;
     pthread_mutex_t *mutex;
 } SmartPtr;
 
-// 스마트 포인터 초기화 함수 (가변 인자 사용)
+#define CREATE_SMART_PTR(type, ...) create_smart_ptr(sizeof(type), __VA_ARGS__)
+
+// 네트워크 정보 구조체
+typedef struct {
+    char ip[INET_ADDRSTRLEN];  // IPv4 주소를 저장
+    sa_family_t family;        // 주소 패밀리 (AF_INET 등)
+} NetworkInfo;
+
+// 네트워크 정보를 얻는 함수
+NetworkInfo get_local_network_info() {
+    struct addrinfo hints, *res;
+    NetworkInfo net_info;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;  // IPv4
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;  // 로컬 호스트를 위한 IP 주소 찾기
+
+    // 호스트 이름을 통해 네트워크 정보 얻기
+    char hostname[256];
+    gethostname(hostname, sizeof(hostname));
+    if (getaddrinfo(hostname, NULL, &hints, &res) != 0) {
+        perror("getaddrinfo 실패");
+        exit(EXIT_FAILURE);
+    }
+
+    // 네트워크 정보 저장 (IPv4 주소)
+    struct sockaddr_in *ipv4 = (struct sockaddr_in *)res->ai_addr;
+    inet_ntop(AF_INET, &(ipv4->sin_addr), net_info.ip, INET_ADDRSTRLEN);
+    net_info.family = res->ai_family;
+
+    freeaddrinfo(res);  // 동적 할당된 메모리 해제
+    return net_info;
+}
+
+// 스마트 포인터 생성 함수 (가변 인자 사용)
 SmartPtr create_smart_ptr(size_t size, ...) {
     SmartPtr sp;
     sp.ptr = malloc(size);
-    if (sp.ptr == NULL) {
-        perror("Failed to allocate memory for smart pointer");
-        exit(EXIT_FAILURE);
-    }
-
     sp.ref_count = (int *)malloc(sizeof(int));
-    if (sp.ref_count == NULL) {
-        perror("Failed to allocate memory for ref_count");
-        free(sp.ptr);
-        exit(EXIT_FAILURE);
-    }
     *(sp.ref_count) = 1;
-
     sp.mutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
-    if (sp.mutex == NULL) {
-        perror("Failed to allocate memory for mutex");
-        free(sp.ptr);
-        free(sp.ref_count);
-        exit(EXIT_FAILURE);
-    }
     pthread_mutex_init(sp.mutex, NULL);
 
-    // 가변 인자를 사용하여 초기화
+    // 가변 인자를 통해 초기화
     va_list args;
     va_start(args, size);
 
     if (size == sizeof(int)) {
         int value = va_arg(args, int);
         *(int *)sp.ptr = value;
-    } else if (size == sizeof(float)) {
-        float value = (float)va_arg(args, double);
-        *(float *)sp.ptr = value;
     } else if (size == sizeof(char) * MAX_STRING_SIZE) {
         const char *str = va_arg(args, const char *);
-        strncpy((char *)sp.ptr, str, MAX_STRING_SIZE);  // 문자열 초기화
+        strncpy((char *)sp.ptr, str, MAX_STRING_SIZE);
     }
 
     va_end(args);
@@ -101,18 +116,34 @@ void release(SmartPtr *sp) {
 
     pthread_mutex_lock(sp->mutex);
     (*(sp->ref_count))--;
+    safe_kernel_printf("Smart pointer released (ref_count: %d)\n", *(sp->ref_count));
+
     if (*(sp->ref_count) == 0) {
         should_free = 1;
+        safe_kernel_printf("Reference count is 0, freeing memory...\n");
+    } else {
+        // Reference count is not 0, do not free memory
+        safe_kernel_printf("Reference count is not 0, not freeing memory\n");
     }
+
     pthread_mutex_unlock(sp->mutex);
 
     if (should_free) {
         free(sp->ptr);
+        sp->ptr = NULL;  // 포인터를 NULL로 설정하여 중복 해제 방지
         free(sp->ref_count);
+        sp->ref_count = NULL;  // ref_count도 NULL로 설정
+
         pthread_mutex_destroy(sp->mutex);
         free(sp->mutex);
+        sp->mutex = NULL;  // mutex 포인터도 NULL로 설정
+        safe_kernel_printf("Memory has been freed\n");
+    } else {
+        safe_kernel_printf("Memory has not been freed\n");
+        kernel_errExit("Memory has not been freed");
     }
 }
+
 
 // 안전한 출력 함수
 static void safe_kernel_printf(const char *format, ...) {
@@ -123,6 +154,10 @@ static void safe_kernel_printf(const char *format, ...) {
     vprintf(format, args);  // 커널 printf 함수 호출
     pthread_mutex_unlock(&print_mutex);  // 출력 뮤텍스 해제
 
+    if(errno != 0) {
+        kernel_errExit("Failed to print message");
+    }
+
     va_end(args);
 }
 
@@ -131,30 +166,13 @@ static void kernel_errExit(const char *format, ...) {
     va_list argList;
 
     va_start(argList, format);
-    outputError(true, errno, true, format, argList);
+    stdout->_IO_write_ptr = stdout->_IO_write_end;  // 출력 버퍼 비우기
+    safe_kernel_printf("ERROR: %s\n", format);
+    fprintf(stderr, "errno: %d (%s)\n", errno, strerror(errno));
+    fflush(stdout);
     va_end(argList);
 
     terminate(true);  // 프로그램 종료
-}
-
-static void outputError(bool useErr, int err, bool flushStdout, const char *format, va_list ap) {
-    char buf[BUF_SIZE], userMsg[BUF_SIZE], errText[BUF_SIZE];
-
-    vsnprintf(userMsg, BUF_SIZE, format, ap);
-
-    if (useErr)
-        snprintf(errText, BUF_SIZE, " [%s %s]",
-                 (err > 0 && err <= MAX_ENAME) ? ename[err] : "?UNKNOWN?", strerror(err));
-    else
-        snprintf(errText, BUF_SIZE, ":");
-
-    snprintf(buf, BUF_SIZE, "ERROR%s %s\n", errText, userMsg);
-
-    if (flushStdout)
-        fflush(stdout);
-
-    fputs(buf, stderr);
-    fflush(stderr);
 }
 
 // 종료 처리
@@ -169,24 +187,38 @@ static void terminate(bool useExit3) {
         _exit(EXIT_FAILURE);
 }
 
-// 스레드 함수 예제
-static void* thread_function(void* arg) {
+// 스레드에서 사용할 함수 예시
+void* thread_function(void* arg) {
     int thread_num = *((int*)arg);
-    safe_kernel_printf("Thread %d: 시작\n", thread_num);
+
+    // 네트워크 정보 가져오기
+    NetworkInfo net_info = get_local_network_info();
+
+    // 스레드 시작 메시지
+    safe_kernel_printf("Thread %d: 시작 - 로컬 IP 주소: %s\n", thread_num, net_info.ip);
+
     sleep(1); // 작업을 모방하기 위한 대기 시간
-    safe_kernel_printf("Thread %d: 종료\n", thread_num);
+
+    // 스레드 종료 메시지
+    safe_kernel_printf("Thread %d: 종료 - 주소 패밀리: %d\n", thread_num, net_info.family);
     return NULL;
 }
 
 // 소켓 통신 함수
 static void kernel_socket_communication(int sock_fd, const char *message, char *response, size_t response_size) {
     if (write(sock_fd, message, strlen(message)) == -1) {
-        printf("Failed to send message through socket");
+        safe_kernel_printf("Failed to send message through socket");
+    } else {
+        safe_kernel_printf("Failed to send message through socket");
+        kernel_errExit("Failed to send message through socket");
     }
 
     ssize_t bytes_read = read(sock_fd, response, response_size - 1);
     if (bytes_read == -1) {
-        printf("Failed to receive message from socket");
+        safe_kernel_printf("Failed to receive message from socket");
+        kernel_errExit("Failed to receive message from socket");
+    } else {
+        kernel_errExit("Failed to receive message from socket");
     }
     response[bytes_read] = '\0';
 }
@@ -195,7 +227,10 @@ static void kernel_socket_communication(int sock_fd, const char *message, char *
 static void kernel_wait_for_process(pid_t pid) {
     int status;
     if (waitpid(pid, &status, 0) < 0) {
-        printf("Failed to wait for process");
+        safe_kernel_printf("Failed to wait for process");
+        kernel_errExit("Failed to wait for process");
+    } else {
+        safe_kernel_printf("Child process exited with status %d\n", status);
     }
 }
 
@@ -203,7 +238,10 @@ static void kernel_wait_for_process(pid_t pid) {
 static void kernel_create_thread(pthread_t *thread, void *(*start_routine)(void *), void *arg) {
     int err = pthread_create(thread, NULL, start_routine, arg);
     if (err != 0) {
-        printf("Failed to create thread");
+        safe_kernel_printf("Failed to create thread");
+        kernel_errExit("Failed to create thread");
+    } else {
+        safe_kernel_printf("Thread created successfully\n");
     }
 }
 
@@ -211,6 +249,11 @@ static void kernel_create_thread(pthread_t *thread, void *(*start_routine)(void 
 static void kernel_join_thread(pthread_t thread) {
     int err = pthread_join(thread, NULL);
     if (err != 0) {
-        printf("Failed to join thread");
+        safe_kernel_printf("Failed to join thread");\
+        kernel_errExit("Failed to join thread");
+    } else {
+        safe_kernel_printf("Thread joined successfully\n");
     }
 }
+
+
